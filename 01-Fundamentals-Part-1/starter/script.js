b1078ca3ced1cf127b55a8ca0ba63f8bb95f5370
minhasnu <minhas.naheem.in@gmail.com>
2020-11-04 04:49:43 +0530

    variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,3 +4,13 @@ console.log(firstName);
 
 var lastName = "naheem";
 var age = 28;
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log("when value is not assingen it shows")
+console.log(job);
+
+job = 'programmer';
+console.log(job);
