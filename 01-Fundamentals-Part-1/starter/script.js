2ac9f5d4662038fe3d3ab19d9c7a767170e5e520
minhasnu <minhas.naheem.in@gmail.com>
2020-11-04 05:18:54 +0530

    operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,6 @@
+/*
+variables and datatypes 
+
 console.log("Hello world!!!");
 var firstName = "Minhas";
 console.log(firstName);
@@ -14,3 +17,48 @@ console.log(job);
 
 job = 'programmer';
 console.log(job);
+*/
+
+/*********************
+ * variable mutation and type coercion
+ 
+
+var name = "Nick";
+var age = 28;
+// Type coercion
+console.log(name + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = true;
+console.log(name + ' is a ' + job + ' and is '+ age + ' old. ');
+
+//Variable mutation
+age = 'twenty eight';
+console.log(age);
+
+alert(name + ' is a ' + job + ' and is '+ age + ' old. ');
+
+var lastName = prompt('What is his last name? ');
+console.log(name + ' ' +lastName);
+*/
+
+/*****************
+ * Basic Operators
+ */
+ageJohn = 19;
+ageNick = 28;
+ var now = 2020
+
+ var yearNick = now - ageNick;
+ var yearjohn = now - ageNick;
+ console.log('Birth yr of nick '+ yearNick);
+ console.log('Birth yr of john '+ yearjohn);
+
+ //Logical Operators
+
+var johnOlder = ageJohn > ageNick;
+
+console.log('is john older than nick' + johnOlder);
+
+//typeof Operators
