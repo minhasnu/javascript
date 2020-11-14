970790b83814d30c94db08cbacfe0d24d9c5b516
minhasnu <minhas.naheem.in@gmail.com>
2020-11-14 07:16:35 +0530

    html mdn into and coding challenge 6

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-var scores, roundScore,activePlayer,dice,gamePlaying;
+var scores, roundScore, activePlayer , gamePlaying, secondTurn, dice;
 init();
 document.querySelector('#current--'+activePlayer).textContent = dice;
 //document.querySelector('#current--'+activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -9,23 +9,38 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
 
     if(gamePlaying){
          //get a random number
-        var dice = Math.floor(Math.random()*6) + 1;
+        var prevDice;
+        
+        if(secondTurn){
+            prevDice = dice;
+            
+        } else {
+            secondTurn = true;     
+        }
 
+        dice = Math.floor(Math.random()*6) + 1;
+        
+        console.log(dice,prevDice);
         //display the number on the dice
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
-        // add the number to the score if the number is not 1
 
-        if (dice !== 1) {
-            //add value and active player remains the same
-            roundScore += dice;
-            document.getElementById('current--'+activePlayer).textContent = roundScore;
+        switch(true) {
+            case dice === 1 :
+                nextPlayer();    
+                break;
+            case dice == 6 && prevDice == 6:
+                // if current roll and previous roll is 6 then the total score is lost and the game goes to the next player
+                scores[activePlayer] = 0;
+                document.getElementById('score--'+activePlayer).textContent = '0';
 
-        } else {
-            // current score becomes zero and then the player is toggled
-            nextPlayer();        
+                nextPlayer();
+            default:
+                roundScore += dice;
+                document.getElementById('current--'+activePlayer).textContent = roundScore;
+
         }
         
 
@@ -37,15 +52,21 @@ document.querySelector('.btn--hold').addEventListener('click',function() {
     scores[activePlayer] += roundScore;
     document.getElementById('score--'+activePlayer).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 20){
+    var input,winningScore;
+    input = document.querySelector('.final-score').value ;
+
+    if(input){
+        winningScore = input;
+    } else {
+        winningScore = 100;
+    }
+
+    if(scores[activePlayer] >= winningScore){
         console.log('player '+activePlayer + ' won ');
         document.querySelector('#name--'+activePlayer).textContent = 'WINNER';
-        scores = [0,0];
         roundScore = 0;
         activePlayer = 0;
         document.querySelector('.dice').style.display = 'none';
-        document.getElementById('score--0').textContent = '0';
-        document.getElementById('score--1').textContent = '0';
         document.getElementById('current--1').textContent = '0';
         document.getElementById('current--0').textContent = '0';
 
@@ -69,10 +90,15 @@ function init() {
     document.getElementById('score--0').textContent = '0';
     document.getElementById('score--1').textContent = '0';
     document.getElementById('current--1').textContent = '0';
+    
     document.getElementById('current--0').textContent = '0';
     document.getElementById('name--0').textContent = 'Player 1';
     document.getElementById('name--1').textContent = 'Player 2';
+    document.querySelector('.player--0').classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
     gamePlaying = true;
+    secondTurn = false;
+
 
 
 }
@@ -84,5 +110,6 @@ function nextPlayer() {
     document.querySelector('.player--0').classList.toggle('player--active');
     document.querySelector('.player--1').classList.toggle('player--active');
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;  
+    secondTurn = false;
 
 }
